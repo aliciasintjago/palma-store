0f49b52bab44ed500e3208d8b06204eb8104d0d7
aliciasintjago <96748702+aliciasintjago@users.noreply.github.com>
2021-12-30 00:47:38 -0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -70,7 +70,7 @@ const Logo = styled.div`
             display: flex;
             padding-right: 10.7rem;
             width: 150px;
-            transform: translateX(-1.3rem);
+            transform: translateX(-0.5rem);
         }
     }  
 `   
